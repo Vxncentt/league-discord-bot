fddd2cb587051adb0a2bc6b645c0ddeac92c0d33
Vxncentt <vxncent.agency@gmail.com>
2025-12-01 23:32:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import express from "express";
-import "./index.js"; // starts the Discord bot
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix ES module paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Import bot
+import(path.join(__dirname, "index.js"));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
