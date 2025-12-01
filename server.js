1822acaa14eaa738bff0b06902af2aa6df05184e
Vxncentt <vxncent.agency@gmail.com>
2025-12-01 23:06:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Bot is running!");
 });
 
-app.listen(3000, () => {
-  console.log("Web server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
 });
